387891278faf7705336c5de371681e2102113af7
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-12-19 10:02:33 +0100

    Pantalla para capturar ingresos y egresos

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
-  styleUrl: './ingreso-egreso.component.css'
+  styleUrl: './ingreso-egreso.component.css',
 })
-export class IngresoEgresoComponent {
+export class IngresoEgresoComponent implements OnInit {
+  ingresoFrom!: FormGroup;
+  tipo: string = 'ingreso';
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.ingresoFrom = this.fb.group({
+      descripcion: ['', Validators.required],
+      monto: ['', Validators.required],
+    });
+  }
+
+  guardar() {
+    if (this.ingresoFrom.invalid) return;
+    console.log(this.ingresoFrom.value);
+    console.log(this.tipo);
+  }
 }
